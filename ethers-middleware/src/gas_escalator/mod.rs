1f1af008bf95f6787f18e916afc3efc227fe68f2
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-05-27 17:51:36 +0100

    chore: make "escalated" log `debug` instead of `trace`

--- ethers-middleware/src/gas_escalator/mod.rs
+++ ethers-middleware/src/gas_escalator/mod.rs
@@ -202,7 +202,7 @@ where
                         {
                             Ok(new_tx_hash) => {
                                 let new_tx_hash = *new_tx_hash;
-                                tracing::trace!(
+                                tracing::debug!(
                                     old_tx_hash = ?tx_hash,
                                     new_tx_hash = ?new_tx_hash,
                                     old_gas_price = ?old_gas_price,
