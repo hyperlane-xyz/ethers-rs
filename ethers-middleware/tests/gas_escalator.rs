ec0f7595410340ebe2dec6b248536d0566c76558
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-06-13 14:50:17 +0100

    chore: merge upstream gas escalator and run test

--- ethers-middleware/tests/gas_escalator.rs
+++ ethers-middleware/tests/gas_escalator.rs
@@ -1,40 +1,41 @@
 #![cfg(not(target_arch = "wasm32"))]
-use ethers_core::types::*;
+use std::convert::TryFrom;
+
+use ethers_core::{types::*, utils::Anvil};
 use ethers_middleware::{
     gas_escalator::{Frequency, GasEscalatorMiddleware, GeometricGasPrice},
-    signer::SignerMiddleware,
+    SignerMiddleware,
 };
-use ethers_providers::Middleware;
+use ethers_providers::{Http, Middleware, Provider};
 use ethers_signers::{LocalWallet, Signer};
-use std::time::Duration;
 
 #[tokio::test]
-#[ignore]
 async fn gas_escalator_live() {
-    // connect to ropsten for getting bad block times
-    let provider = ethers_providers::ROPSTEN.ws().await;
-    let provider = provider.interval(Duration::from_millis(2000u64));
-    let wallet = "fdb33e2105f08abe41a8ee3b758726a31abdd57b7a443f470f23efce853af169"
-        .parse::<LocalWallet>()
-        .unwrap();
+    let anvil = Anvil::new().block_time(2u64).spawn();
+    let chain_id = anvil.chain_id();
+    let provider = Provider::<Http>::try_from(anvil.endpoint()).unwrap();
+
+    // wrap with signer
+    let wallet: LocalWallet = anvil.keys().first().unwrap().clone().into();
+    let wallet = wallet.with_chain_id(chain_id);
     let address = wallet.address();
     let provider = SignerMiddleware::new(provider, wallet);
 
-    let escalator = GeometricGasPrice::new(5.0, 10u64, Some(2_000_000_000_000u64));
-
-    let provider = GasEscalatorMiddleware::new(provider, escalator, Frequency::Duration(3000));
+    // wrap with escalator
+    let escalator = GeometricGasPrice::new(5.0, 1u64, Some(2_000_000_000_000u64));
+    let provider = GasEscalatorMiddleware::new(provider, escalator, Frequency::Duration(300));
 
     let nonce = provider.get_transaction_count(address, None).await.unwrap();
-    let tx = TransactionRequest::pay(Address::zero(), 1u64).gas_price(10_000_000);
+    // 1 gwei default base fee
+    let gas_price = U256::from(1_000_000_000_u64);
+    // 125_000_000_000
+    let tx = TransactionRequest::pay(Address::zero(), 1u64)
+        .gas_price(gas_price)
+        .nonce(nonce)
+        .chain_id(chain_id);
 
-    // broadcast 3 txs
-    provider.send_transaction(tx.clone().nonce(nonce), None).await.unwrap();
-    provider.send_transaction(tx.clone().nonce(nonce + 1), None).await.unwrap();
-    provider.send_transaction(tx.clone().nonce(nonce + 2), None).await.unwrap();
-
-    // Wait a bunch of seconds and refresh etherscan to see the transactions get bumped
-    tokio::time::sleep(std::time::Duration::from_secs(100)).await;
-
-    // TODO: Figure out how to test this behavior properly in a local network. If the gas price was
-    // bumped then the tx hash will be different
+    // regardless of whether we get a receipt here, if gas is escalated by sending a new tx, this receipt won't be useful,
+    // since a tx with a different hash will end up replacing
+    let _pending = provider.send_transaction(tx, None).await.expect("could not send").await;
+    // checking the logs shows that the gas price is indeed being escalated twice
 }
