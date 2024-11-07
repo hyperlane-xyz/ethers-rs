7fa674e8d76169524922fa0a96e3ca7698c49ee2
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2024-11-07 18:35:41 +0000

    fix: don't escalate if you've already escalated

--- ethers-middleware/tests/gas_escalator.rs
+++ ethers-middleware/tests/gas_escalator.rs
@@ -13,7 +13,7 @@ use tokio::time::sleep;
 
 #[tokio::test]
 async fn gas_escalator_live() {
-    let anvil = Anvil::new().port(8545u16).block_time(2u64).spawn();
+    let anvil = Anvil::new().port(8545u16).block_time(10u64).spawn();
     let chain_id = anvil.chain_id();
     let provider = Provider::<Http>::try_from(anvil.endpoint()).unwrap();
 
@@ -24,8 +24,23 @@ async fn gas_escalator_live() {
     let provider = SignerMiddleware::new(provider, wallet);
 
     // wrap with escalator
-    let escalator = GeometricGasPrice::new(5.0, 1u64, Some(2_000_000_000_000u64));
+    // escalate every 2 seconds. We should only see 4-5 escalations in total
+    let escalator = GeometricGasPrice::new(1.1, 2u64, Some(2_000_000_000_000u64));
     let provider = GasEscalatorMiddleware::new(provider, escalator, Frequency::Duration(300));
+
+    // set the gas price to 10 gwei, so we need to escalate twice
+    // this works but the tx still goes through regardless of its gas price for some reason
+    // reqwest::Client::new()
+    //     .post(&format!("{}/", anvil.endpoint()))
+    //     .json(&json!({
+    //         "jsonrpc": "2.0",
+    //         "method": "anvil_setMinGasPrice",
+    //         "params": [10_000_000_000u64],
+    //         "id": 1
+    //     }))
+    //     .send()
+    //     .await
+    //     .unwrap();
 
     let nonce = provider.get_transaction_count(address, None).await.unwrap();
     // 1 gwei default base fee
@@ -37,6 +52,6 @@ async fn gas_escalator_live() {
 
     let pending = provider.send_transaction(tx, None).await.expect("could not send");
     let receipt = pending.await;
-    sleep(Duration::from_secs(3)).await;
+    sleep(Duration::from_secs(2)).await;
     println!("receipt gas price: , hardcoded_gas_price: {}, receipt: {:?}", gas_price, receipt);
 }
