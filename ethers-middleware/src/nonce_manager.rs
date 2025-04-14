ca74b7ba6cc03d6280dc9c6cbdc8ae21b4b98d49
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2025-04-14 14:36:50 +0100

    more comments

--- ethers-middleware/src/nonce_manager.rs
+++ ethers-middleware/src/nonce_manager.rs
@@ -184,8 +184,11 @@ where
                 // for this tx to land, it needs to have used `onchain_nonce`
                 // but since the internal nonce is incremented by 1 after its value is read,
                 // the nonce used to broadcast the tx was `internal_nonce - 1`.
-                // so we need to check if the onchain nonce is equal to the last used nonce
+                // so we need to check if the onchain nonce is equal to the last used nonce.
                 let last_used_nonce = internal_nonce.saturating_sub(1);
+                // we're using `last_used_nonce` instead of `tx.nonce()`  because the use of atomics
+                // in the NonceManager suggests it could be used to send txs in parallel,
+                // so tx.nonce() may not be the last used nonce
                 if onchain_nonce_u64 != last_used_nonce {
                     self.nonce.store(onchain_nonce.as_u64(), Ordering::SeqCst);
                     // the nonce was just resynced so this counter can be reset
